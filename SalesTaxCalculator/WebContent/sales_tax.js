89729c9ebde6ce48fe14f15b204f13dc77b5d46d
AnkushiS <ankushi.sharma@infosys.com>
2015-08-18 11:26:01 -0400

    changes made for caluclate_click

--- SalesTaxCalculator/WebContent/sales_tax.js
+++ SalesTaxCalculator/WebContent/sales_tax.js
@@ -1,4 +1,33 @@
 var $ = function (id) {
+	
     return document.getElementById(id); 
 }
 
+var calculate_click = function(){
+	var subtotal = parseFloat($("subtotal").value);
+	var tax = parseFloat($("taxRate").value);
+	
+	$("salesTax").value="";
+	$("total").value="";
+	
+	if(isNaN(subtotal) || subtotal < 0){
+		alert("Subtoal shoudl be a number");
+	}else if(isNaN(tax) ||tax<0){
+		alert("tax rate should be a number");
+	}else {
+		
+		var salesTax = subtotal * (tax/100);
+		salesTax = parseFloat(salesTax.toFixed(2));
+		var total = salesTax + subtotal;
+				
+		
+		$("salesTax").value = salesTax;
+		$("total").value = total.toFixed(2);			
+	}
+}
+
+
+ window.onload = function () {
+	 $("calculate").onclick = calculate_click;
+	 $("subtotal").focus;
+}
